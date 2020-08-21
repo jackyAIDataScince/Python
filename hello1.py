f9766558ef1446acff50b9eb8ff31c264fbafd59
jacky-hello-cck <69745867+jacky-hello-cck@users.noreply.github.com>
2020-08-21 06:55:13 +0000

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,6 +1,6 @@
 #import necessary modules
 import csv
-with open('/home/jackyjcck/hello/data.csv','rt')as f:
+with open('/file/data.csv','rt')as f:
   data = csv.reader(f)
   for row in data:
         print(row)
